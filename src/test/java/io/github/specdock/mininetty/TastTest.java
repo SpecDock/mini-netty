ba86e0031de5dc20004a28fe8be6655933e504e3
specter <2928781682@qq.com>
2026-02-24 22:42:51 +0800

    将DefaultChannelHandlerContext大部分内容提炼到AbstractChannelHandlerContext类，并且在DefaultChannelPipeline类中新增HeadContext和TailContext内部类，并且初步跑通pipeline（通过ServerChannel的Pipeline给SocketChannel的PipeLine添加ChannelInitializer<SocketChannel>）

--- src/test/java/io/github/specdock/mininetty/TastTest.java
+++ src/test/java/io/github/specdock/mininetty/TastTest.java
@@ -1,7 +1,12 @@
 package io.github.specdock.mininetty;
 
 import io.github.specdock.mininetty.bootstrap.ServerBootstrap;
+import io.github.specdock.mininetty.channel.ChannelHandler;
+import io.github.specdock.mininetty.channel.ChannelHandlerContext;
+import io.github.specdock.mininetty.channel.ChannelInitializer;
+import io.github.specdock.mininetty.channel.SimpleChannelInboundHandler;
 import io.github.specdock.mininetty.channel.nio.NioEventLoopGroup;
+import io.github.specdock.mininetty.channel.socket.SocketChannel;
 import io.github.specdock.mininetty.channel.socket.nio.NioServerSocketChannel;
 
 /**
@@ -13,7 +18,36 @@ public class TastTest {
     public static void main(String[] args) {
         ServerBootstrap serverBootstrap = new ServerBootstrap();
         serverBootstrap.group(new NioEventLoopGroup(), new NioEventLoopGroup(3))
-                        .channel(NioServerSocketChannel.class)
-                                .bind(8080);
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<SocketChannel>(){
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline()
+                                .addLast(new ChannelHandler() {
+                                    @Override
+                                    public void channelRegistered(ChannelHandlerContext ctx) {
+
+                                    }
+
+                                    @Override
+                                    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+
+                                        ctx.fireChannelRead(msg);
+                                    }
+                                });
+                    }
+
+                    @Override
+                    public void channelRegistered(ChannelHandlerContext ctx) {
+
+                    }
+
+                    @Override
+                    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+                        System.out.println("----------------------我是ChannelInitializer");
+                        ctx.fireChannelRead(msg);
+                    }
+                })
+                .bind(8080);
     }
 }
